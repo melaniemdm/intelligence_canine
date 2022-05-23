a7199322ea4016c3afb0b6ec98e5a468e4aace11
melaniemdm <melanieborja.pro@gmail.com>
2022-05-23 16:31:33 +0200

    feature header et navigate

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import './style.scss';
+import logoImg from '../../assets/logo.png';
+import Navigate from '../Navigate'
+
+function Header(){
+
+    return (
+        <header>
+        <div className="containerHeader">
+            <div className="containerLogo">
+                <img src={logoImg} alt="logo intelligence Canine" className="logoImg"/>
+               <div className="nom">Intelligence Canine </div> 
+            </div>
+            <Navigate/>
+             </div></header>
+    )
+}
+export default Header
---
+++ src/components/Navigate/index.js
@@ -0,0 +1,20 @@
+import './style.scss';
+
+function Navigate(){
+
+    return(
+        <div className='containerNav'>
+<div className="container black circleBehind">
+  <a href=" ">HOME</a>
+  <a href=" ">ARTICLES</a>
+  <a href=" ">PORTFOLIO</a>
+  <a href=" ">ABOUT</a>
+  <a href=" ">CONTACT</a>
+</div>
+
+
+
+        </div>
+    )
+}
+export default Navigate
