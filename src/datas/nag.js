804316a133f01a317d0245f272e4acd15ce502b5
melaniemdm <melanieborja.pro@gmail.com>
2022-05-24 21:15:09 +0200

    add(img) datas

--- src/datas/nag.js
+++ src/datas/nag.js
@@ -9,6 +9,17 @@ export const nagano=
         'assets/chaise.jpg',
         'assets/ombre.jpg',
         'assets/love.jpg',
+        'assets/love2.jpg',
+        'assets/ride.jpg',
+        'assets/kiss.jpg',
+        'assets/prof.jpg',
+        'assets/prof2.jpg',
+        'assets/ombre2.jpg',
+        'assets/ombre3.jpg',
+        'assets/nag.jpg',
+        'assets/check.jpg',
+        'assets/love3.jpg',
+        'assets/sprint.jpg',
     ]
 }
 
