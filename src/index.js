1a8f768df59a277fda2467f0f2d6473eb30f966a
melaniemdm <melanieborja.pro@gmail.com>
2022-06-02 17:19:25 +0200

    add :schroll top

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import ScrollToTop from "react-scroll-to-top";
 import Header from '../src/components/Header';
 import Banner from '../src/components/Banner';
 import Prestations from '../src/components/Prestations';
@@ -11,17 +12,21 @@ import Contact from '../src/components/Contact';
 import Footer from '../src/components/Footer';
 import reportWebVitals from './reportWebVitals';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <div>
   <Header/>
   <Banner/>
+ 
   <Prestations/>
   <Pro/>
   <Particulier/>
   <Avis/>
   <Contact/>
   <Footer/>
+ <ScrollToTop /></div>
    </React.StrictMode>
 );
 
