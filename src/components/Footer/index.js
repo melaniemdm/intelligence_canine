88a49483bd2656db1f8506bb70aea2461c0db51b
melaniemdm <melanieborja.pro@gmail.com>
2022-06-02 17:22:15 +0200

    add(js) lien facebook footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -18,9 +18,11 @@ function Footer(){
   <a href="#contactMe">Me contacter</a>
 </div>
 <div className="textFooter">
+<a href="https://www.facebook.com/Intelligencecanine">
 <img src="/assets/facebook.png" alt="lien facebook" className="logoFacebook"/>
 <div>&nbsp; Abonnez vous à la page facebook et venez voir par vous même  ! &nbsp;</div>
-<img src="/assets/like.png" alt="lien facebook" className="like"/>
+<img src="/assets/like.png" alt="lien facebook" className="like"/></a>
+
 </div>
 </div>
         </footer>
