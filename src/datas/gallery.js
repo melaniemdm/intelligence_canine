4e8d3e71a0de261168a29e5abf9f232b34e5afc2
melaniemdm <melanieborja.pro@gmail.com>
2022-06-13 17:31:42 +0200

    feature(js) gallery

---
+++ src/datas/gallery.js
@@ -0,0 +1,17 @@
+export const picturesGallery = [
+    {
+        idPictures:'1',
+      original: 'https://picsum.photos/id/1018/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1018/250/150/',
+    },
+    {
+        idPictures:'2',
+      original: 'https://picsum.photos/id/1015/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1015/250/150/',
+    },
+    {
+        idPictures:'3',
+      original: 'https://picsum.photos/id/1019/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1019/250/150/',
+    },
+  ];
--- src/pages/Gallery/index.js
+++ src/pages/Gallery/index.js
@@ -1,24 +1,18 @@
 import ImageGallery from 'react-image-gallery';
+import {picturesGallery} from '../../datas/gallery';
+import './style.scss';
+import "react-image-gallery/styles/css/image-gallery.css";
 
 
-const PicturesGallery = [
-    {
-      original: 'https://picsum.photos/id/1018/1000/600/',
-      thumbnail: 'https://picsum.photos/id/1018/250/150/',
-    },
-    {
-      original: 'https://picsum.photos/id/1015/1000/600/',
-      thumbnail: 'https://picsum.photos/id/1015/250/150/',
-    },
-    {
-      original: 'https://picsum.photos/id/1019/1000/600/',
-      thumbnail: 'https://picsum.photos/id/1019/250/150/',
-    },
-  ];
   
   function Gallery() {
-    return( <div>
-      return <ImageGallery items={PicturesGallery} />;</div>
+    return( <div className="containerGallery">
+    <ImageGallery  showBullets='true' items={picturesGallery} />
+
+
+    
+    
+    </div>
     )
   }
   export default Gallery;
