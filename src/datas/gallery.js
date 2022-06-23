8f4c39accb00af4a7655cad8495daadd7534a7bf
melaniemdm <melanieborja.pro@gmail.com>
2022-06-23 21:51:57 +0200

    feature(imgcss-js) gallery

--- src/datas/gallery.js
+++ src/datas/gallery.js
@@ -1,17 +1,31 @@
 export const picturesGallery = [
-    {
-        idPictures:'1',
-      original: 'https://picsum.photos/id/1018/1000/600/',
-      thumbnail: 'https://picsum.photos/id/1018/250/150/',
-    },
-    {
-        idPictures:'2',
-      original: 'https://picsum.photos/id/1015/1000/600/',
-      thumbnail: 'https://picsum.photos/id/1015/250/150/',
-    },
-    {
-        idPictures:'3',
-      original: 'https://picsum.photos/id/1019/1000/600/',
-      thumbnail: 'https://picsum.photos/id/1019/250/150/',
-    },
-  ];
+  {
+    idPictures: "1",
+    original: process.env.PUBLIC_URL + "/assets/apache.jpg",
+  },
+  {
+    idPictures: "2",
+    original: process.env.PUBLIC_URL + "/assets/hoppy.jpg",
+  },
+  {
+    idPictures: "3",
+    original: process.env.PUBLIC_URL + "/assets/nagEntretien.jpg",
+  },
+  {
+    idPictures: "4",
+    original: process.env.PUBLIC_URL + "/assets/test.mp4",
+  },
+  {
+    idPictures: "5",
+    original: process.env.PUBLIC_URL + "/assets/ranger.jpg",
+  },
+  {
+    idPictures: "6",
+    original: process.env.PUBLIC_URL + "/assets/rolex.jpg",
+  },
+  {
+    idPictures: "7",
+    original: process.env.PUBLIC_URL + "/assets/ryo.jpg",
+  },
+  
+];
--- src/pages/Gallery/index.js
+++ src/pages/Gallery/index.js
@@ -1,18 +1,45 @@
-import ImageGallery from 'react-image-gallery';
+
 import {picturesGallery} from '../../datas/gallery';
 import './style.scss';
-import "react-image-gallery/styles/css/image-gallery.css";
 
 
   
-  function Gallery() {
-    return( <div className="containerGallery">
-    <ImageGallery  showBullets='true' items={picturesGallery} />
+  // function Gallery() {
+  //   return( <div className="containerGallery">
+  //    items={picturesGallery} 
 
 
     
     
-    </div>
-    )
-  }
-  export default Gallery;
+  //   </div>
+  //   )
+  // }
+  // export default Gallery;
+
+  import React from 'react'
+import Carousel from 'better-react-carousel'
+
+const Gallery = () => {
+  return (<div className="containerGallery">
+  <Carousel cols={2} rows={1} gap={10} loop >
+      { picturesGallery.map((picture, index) => {
+        if (picture.original.includes("mp4")){
+      //video
+      return <Carousel.Item>
+      <video className='videoDog' controls autoplay="true">
+        <source  height="100%" src={picture.original}
+        type="video/mp4" />
+        Sorry, your browser doesn't support embedded videos.
+      </video>
+    </Carousel.Item>} 
+    
+    //image
+      return <Carousel.Item>
+       <img className='imgDog' width="100%" src={picture.original} alt=""/>
+     </Carousel.Item>
+            
+      }) }
+ 
+    </Carousel></div>)
+}
+export default Gallery;
