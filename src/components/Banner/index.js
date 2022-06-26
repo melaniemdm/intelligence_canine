543dc4b10fb456bf2ae56114b27ce1d4922c15ff
melaniemdm <melanieborja.pro@gmail.com>
2022-06-26 22:28:51 +0200

    fix(banner) delete (file) particulier - refacto

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,23 +1,18 @@
 import './style.scss';
-import {nagano} from '../../datas/nag';
-
 import React from 'react';
 import { Slide   } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css';
 
 
-console.log(nagano)
-
-function Banner(){
+function Banner(props){
  
-    const images = nagano.picturesBanner;
-    
+   
    
         return (
           <div className="slide-container">
             <Slide   duration={1000} transitionDuration={3000}  cssClass='slideShow'  arrows={false} slidesToShow={1}>
               {
-                images.map((each, index) => <img key={index} style={{width: "100%"}} src={process.env.PUBLIC_URL +'/'+ each} alt="images de nagano " className="slideShowImg"/>)
+                props.data.map((each, index) => <img key={index} style={{width: "100%"}} src={process.env.PUBLIC_URL +'/'+ each} alt="images de chiens " className="slideShowImg"/>)
               }
             </Slide>
            
--- src/pages/Accueil/index.js
+++ src/pages/Accueil/index.js
@@ -3,18 +3,19 @@ import Banner from "../../components/Banner";
 import Presentation from "../../components/Presentation";
 import Services from "../../components/Services";
 import Pro from "../../components/Pro";
-import Particulier from "../../components/Particuliers";
+import {nagano} from '../../datas/nag';
+import {particuliers} from '../../datas/particuliers';
 import Avis from "../../components/Avis";
 import Contact from "../../components/Contact";
 
 function Accueil() {
   return (
     <div>
-      <Banner />
+      <Banner data={nagano.picturesBanner}/>
       <Presentation />
       <Services />
       <Pro />
-      <Particulier />
+      <Banner  data={particuliers.picturesBannerPart}/>
       <Avis />
       <Contact />
       <ScrollToTop />
