04814f17a6000a71a258d74a1982899165324ab9
melaniemdm <melanieborja.pro@gmail.com>
2022-06-27 10:53:52 +0200

    fix(css & js) navigate responsive

--- src/components/Navigate/index.js
+++ src/components/Navigate/index.js
@@ -8,12 +8,12 @@ function Navigate(){
 <div className="container black circleBehind">
 
 <nav className="navigate">
-      <Link to="/accueil" className="accueil">ACCUEIL</Link> 
-      <Link to="/accueil#services" className="services">SERVICES</Link>
-      <Link to="/accueil#pro" className="pro">PROFESSIONNELS</Link>
-      <Link to="/accueil#particuliers" className="particuliers">PARTICULIERS</Link>
-      <Link to="/gallery" className="gallery">GALLERY</Link>
-      <Link to="/accueil#contactMe" className="contact">CONTACT</Link>
+     <div className="navItem"><Link to="/accueil" className="accueil">ACCUEIL</Link> </div>
+     <div className="navItem"> <Link to="/accueil#services" className="services">SERVICES</Link></div>
+     <div className="navItem"> <Link to="/accueil#pro" className="pro">PROFESSIONNELS</Link></div>
+     <div className="navItem"> <Link to="/accueil#particuliers" className="particuliers">PARTICULIERS</Link></div>
+     <div className="navItem"> <Link to="/gallery" className="gallery">GALLERY</Link></div>
+     <div className="navItem"> <Link to="/accueil#contactMe" className="contact">CONTACT</Link></div>
     </nav>
 
  
