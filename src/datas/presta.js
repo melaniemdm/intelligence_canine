5257f1d7f685e0dcf32db50b963b545e77f8e28b
melaniemdm <melanieborja.pro@gmail.com>
2022-06-29 22:48:39 +0200

    fix(data) services 4 & 5

--- src/datas/presta.js
+++ src/datas/presta.js
@@ -1,39 +1,92 @@
 export const nosPrestations = [
   {
     idService: "1",
-    titlePresta: "Education du chiot",
+    titlePresta: "Éducation du chiot et du jeune chien ",
     prix: "sur devis",
     detail:"Parce que l'éducation se commence dès le plus jeune age, commencer l'éducation chiot permet d'éviter le plus souvent des problemes de comportement. ",
   },
   {
     idService: "2",
-    titlePresta: "Ré-éducation du chien adulte",
-    prix: "sur devis",
+    titlePresta: "rééducation du chien adulte et troubles de comportement ",
+    prix: "Programme personnalisé ",
     detail:"Cette formation s'adresseaux personnes qui se sentent débordées et qui souahaitent améliorer leur relation avec leur animal.",
   },
   {
     idService: "3",
-    titlePresta: "Balade éducative",
+    titlePresta: "Conseils pour vous aider à choisir votre compagnon à 4 pattes",
     prix: "sur demande",
-    detail:"Les balades educatives sont très intructives et ludiques pour le binome chien-maitre. Sur inscription uniquement, les annonces des balades sont directement annoncées sur le faceboock. Les chiens réactifs sont acceptés uniquement s'ils ont suivi des cours au préalable avec Intelligence Canice.",
+    detail:"Conseils sur la race et tests  de caractère sur la portée.",
   },
   {
     idService: "4",
-    titlePresta: "Troubles du comportement chez le chien",
+    titlePresta: "Tests de caractères à destination des éleveurs ",
     prix: "sur devis",
-    detail:"Cette formation s'adresse a toutes le personne qui souhaite comprendre, aider son animal.",
+    detail:["Placer le bon chiot dans la bonne famille, un bonheur assuré pour tous.",<br></br>,
+    "Déjà plus d’une centaine de tests réalisés avec une très grande satisfaction  des professionnels qui m’ont fait confiance",<br></br>,
+    "Modalités organisationnelles", <br></br>,
+    "Âge des chiots entre 5 et 7 semaines", <br></br>,
+    "Un terrain ou une pièce non exploré par les chiots et cela en fonction des conditions climatiques, sera nécessaire pour la réalisation des tests.",<br></br>,
+    "Tarifs appliqués et factures selon nombre de chiots et distance kilométrique parcourue",<br></br>,
+    "Une fiche rédigée sur chaque chiot sera transmise à l’éleveur dans les 24h",<br></br>,
+    "Le bon chiot dans la bonne famille pour ke bonheur de tous"],
   },
+  
   {
     idService: "5",
-    titlePresta: "Prévention morsure auprès des enfants",
+    titlePresta: "Formation à la prévention morsure  pour  écoles , centres de loisirs ou sociétés",
     prix: "sur devis",
-    detail:"Cette formation s'adresse aux enfants dans le cadre périscolaires, à des adultes dans le cadre de leur travail (livreurs, facteurs...) ou de leur loisirs (associations...). Pour le loisir et la sécurité et le bien être de tous, il est indispensable de senbsibiliser les enfants au language canin car comme la prévention routière, les bons gestes sont importants.",
+    detail:["Cette formation s’adresse aux enfants dans le cadre périscolaires, à des adultes dans le cadre de leur travail (livreurs, facteurs ..) ou de leur loisir (membres d’associations de sauvetage ou de loisirs  canins ) ." ,<br></br>,
+    "Pourquoi ? ",<br></br>,
+      "Éducateur canin comportementaliste, je suis très sensibilisée  à l’approche du chien par les enfants. En effet, les morsures de chiens sont la 3ème cause des accidents domestiques en France et 50% d’entre elles sont faites au visage chez les 6-10 ans. ",<br></br>,
+      
+     " Pour leur sécurité et le bien être de tous, il est indispensable d’éduquer les enfants au langage canin car comme pour la prévention routière , les bons gestes ça s’apprend. ",<br></br>,
+      
+      "Objectifs pédagogiques", <br></br>,
+      "•Apprendre aux enfants à lire un chien de part sa posture, sa position de queue ",<br></br>,
+      
+     " •Comment prendre contact avec un chien inconnu",<br></br>,
+      "Son approche, son langage ",<br></br>,
+      
+      "•Comment caresser correctement un chien sans lui envoyer de mauvais messages ",<br></br>,
+      
+      "•Comment réagir face à un chien agressif ou brutal ",<br></br>,
+      
+      "•Comment jouer avec un chien sans danger et dans le respect de l’animal afin de développer une bonne et saine complicité chien/enfant ",<br></br>,
+           
+      "Animation ",<br></br>,
+    " Cette formation s’adresse essentiellement aux enfants de 6 à 11 ans  et aux adultes", <br></br>,
+      "Elle dure entre 1h et 1h30 en fonction de la participation des enfants." ,<br></br>,
+     " Son approche pédagogique est adaptée car ludique et interactive. Des petits exercices de mise en situations sont proposés aux enfants avec la présence de mon chien Nagano . ",<br></br>,
+      
+     " Côté organisationnel ",<br></br>,
+      "Des groupes de 10 enfants maximum ",<br></br>,
+      "Je peux intervenir au maximum 2 fois sur la journée pour une bonne gestion émotionnelle de mon chien.",<br></br>,
+
+    ],
 
   },
   {
     idService: "6",
-    titlePresta: "Cours d'éthologie",
+    titlePresta: "Conférence sur l’éthologie canine , la place du chien au sein de notre foyer ",
     prix: "sur devis",
-    detail:"Cette formation est indispensable afin de comprendre le mode de fonctionnement de son chien. Ell est également un pré requis au bon développement d'une magnifique relation avec son animal.",
+    detail:"Stage : communication canine et technique de relaxation martiale",
+  },
+  {
+    idService: "7",
+    titlePresta: "Balade éducative, énigmatique et très ludique selon les délires de votre serviteur ",
+    prix: "sur devis",
+    detail:" ",
+  },
+  {
+    idService: "8",
+    titlePresta: "Prestations audios ",
+    prix: "sur devis",
+    detail:" ",
+  },
+  {
+    idService: "9",
+    titlePresta: "Déplacement en province",
+    prix: "sur devis",
+    detail:" ",
   },
 ];
