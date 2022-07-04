f9efb5e329e3df25e6416b1e4b71f62b47466290
melaniemdm <melanieborja.pro@gmail.com>
2022-07-04 22:54:38 +0200

    fix(js) add dataimg

--- src/datas/galleryImg.js
+++ src/datas/galleryImg.js
@@ -7,9 +7,13 @@ export const picturesGallery = [
     idPictures: "2",
     original: process.env.PUBLIC_URL + "/assets/hoppy.jpg",
   },
-  {
+    {
     idPictures: "3",
     original: process.env.PUBLIC_URL + "/assets/nagEntretien.jpg",
+  },
+  {
+    idPictures: "4",
+    original: process.env.PUBLIC_URL + "/assets/simone.jpg",
   },
    {
     idPictures: "5",
@@ -23,5 +27,193 @@ export const picturesGallery = [
     idPictures: "7",
     original: process.env.PUBLIC_URL + "/assets/ryo.jpg",
   },
-  
+   {
+    idPictures: "8",
+    original: process.env.PUBLIC_URL + "/assets/nagTata.jpeg",
+  },
+  {
+    idPictures: "9",
+    original: process.env.PUBLIC_URL + "/assets/relax.jpeg",
+  },
+  {
+    idPictures: "10",
+    original: process.env.PUBLIC_URL + "/assets/pupy1.jpeg",
+  },
+  {
+    idPictures: "11",
+    original: process.env.PUBLIC_URL + "/assets/kiss.jpeg",
+  },
+  {
+    idPictures: "12",
+    original: process.env.PUBLIC_URL + "/assets/kiss2.jpeg",
+  },
+  {
+    idPictures: "13",
+    original: process.env.PUBLIC_URL + "/assets/agua.jpeg",
+  },
+  {
+    idPictures: "14",
+    original: process.env.PUBLIC_URL + "/assets/pupy2.jpeg",
+  },
+  {
+    idPictures: "15",
+    original: process.env.PUBLIC_URL + "/assets/pupy3.jpeg",
+  },
+  {
+    idPictures: "16",
+    original: process.env.PUBLIC_URL + "/assets/pupy4.jpeg",
+  },
+  {
+    idPictures: "17",
+    original: process.env.PUBLIC_URL + "/assets/kiss3.jpeg",
+  },
+  {
+    idPictures: "18",
+    original: process.env.PUBLIC_URL + "/assets/sleep.jpeg",
+  },
+  {
+    idPictures: "19",
+    original: process.env.PUBLIC_URL + "/assets/pupy5.jpeg",
+  },
+  {
+    idPictures: "20",
+    original: process.env.PUBLIC_URL + "/assets/cours1.jpeg",
+  },
+  {
+    idPictures: "21",
+    original: process.env.PUBLIC_URL + "/assets/cours2.jpeg",
+  },
+  {
+    idPictures: "22",
+    original: process.env.PUBLIC_URL + "/assets/cours3.jpeg",
+  },
+  {
+    idPictures: "23",
+    original: process.env.PUBLIC_URL + "/assets/kiss4.jpeg",
+  },
+  {
+    idPictures: "24",
+    original: process.env.PUBLIC_URL + "/assets/cours4.jpeg",
+  },
+  {
+    idPictures: "25",
+    original: process.env.PUBLIC_URL + "/assets/cours5.jpeg",
+  },
+  {
+    idPictures: "26",
+    original: process.env.PUBLIC_URL + "/assets/kiss5.jpeg",
+  },
+  {
+    idPictures: "27",
+    original: process.env.PUBLIC_URL + "/assets/kiss6.jpeg",
+  },
+  {
+    idPictures: "28",
+    original: process.env.PUBLIC_URL + "/assets/kiss7.jpeg",
+  },
+  {
+    idPictures: "29",
+    original: process.env.PUBLIC_URL + "/assets/hugs.jpeg",
+  },
+  {
+    idPictures: "30",
+    original: process.env.PUBLIC_URL + "/assets/prof.jpeg",
+  },
+  {
+    idPictures: "31",
+    original: process.env.PUBLIC_URL + "/assets/cours6.jpeg",
+  },
+  {
+    idPictures: "32",
+    original: process.env.PUBLIC_URL + "/assets/cours7.jpeg",
+  },
+  {
+    idPictures: "33",
+    original: process.env.PUBLIC_URL + "/assets/cours8.jpeg",
+  },
+  {
+    idPictures: "34",
+    original: process.env.PUBLIC_URL + "/assets/cours10.jpeg",
+  },
+  {
+    idPictures: "35",
+    original: process.env.PUBLIC_URL + "/assets/cours11.jpeg",
+  },
+  {
+    idPictures: "36",
+    original: process.env.PUBLIC_URL + "/assets/cours12.jpeg",
+  },
+  {
+    idPictures: "37",
+    original: process.env.PUBLIC_URL + "/assets/cours13.jpeg",
+  },
+  {
+    idPictures: "38",
+    original: process.env.PUBLIC_URL + "/assets/kiss8.jpeg",
+  },
+  {
+    idPictures: "39",
+    original: process.env.PUBLIC_URL + "/assets/lol.jpeg",
+  },
+  {
+    idPictures: "40",
+    original: process.env.PUBLIC_URL + "/assets/cours14.jpeg",
+  },
+  {
+    idPictures: "41",
+    original: process.env.PUBLIC_URL + "/assets/cours15.jpeg",
+  },
+  {
+    idPictures: "42",
+    original: process.env.PUBLIC_URL + "/assets/kiss9.jpeg",
+  },
+  {
+    idPictures: "43",
+    original: process.env.PUBLIC_URL + "/assets/kiss10.jpeg",
+  },
+  {
+    idPictures: "44",
+    original: process.env.PUBLIC_URL + "/assets/kiss11.jpeg",
+  },
+  {
+    idPictures: "45",
+    original: process.env.PUBLIC_URL + "/assets/kiss12.jpeg",
+  },
+  {
+    idPictures: "46",
+    original: process.env.PUBLIC_URL + "/assets/hugs2.jpeg",
+  },
+  {
+    idPictures: "47",
+    original: process.env.PUBLIC_URL + "/assets/kiss13.jpeg",
+  },
+  {
+    idPictures: "48",
+    original: process.env.PUBLIC_URL + "/assets/cours16.jpeg",
+  },
+  {
+    idPictures: "49",
+    original: process.env.PUBLIC_URL + "/assets/cours17.jpeg",
+  },
+  {
+    idPictures: "50",
+    original: process.env.PUBLIC_URL + "/assets/cours18.jpeg",
+  },
+  {
+    idPictures: "51",
+    original: process.env.PUBLIC_URL + "/assets/cours19.jpeg",
+  },
+  {
+    idPictures: "52",
+    original: process.env.PUBLIC_URL + "/assets/cours20.jpeg",
+  },
+  {
+    idPictures: "53",
+    original: process.env.PUBLIC_URL + "/assets/pupy6.jpeg",
+  },
+  {
+    idPictures: "54",
+    original: process.env.PUBLIC_URL + "/assets/kiss14.jpeg",
+  },
+
 ];
