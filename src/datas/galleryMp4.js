435176831bdcdf4404998f71daef3afdcdaedc89
melaniemdm <melanieborja.pro@gmail.com>
2022-07-05 11:32:26 +0200

    add(data) mp4

--- src/datas/galleryMp4.js
+++ src/datas/galleryMp4.js
@@ -1,15 +1,25 @@
 export const videosGallery = [
  
   {
-    idPictures: "4",
+    idPictures: "1",
     original: process.env.PUBLIC_URL + "/assets/test.mp4",
   },
   {
-    idPictures: "8",
+    idPictures: "2",
     original: process.env.PUBLIC_URL + "/assets/tataNag.mp4",
   },
   {
-    idPictures: "9",
+    idPictures: "3",
     original: process.env.PUBLIC_URL + "/assets/movi.mp4",
   },
+  {
+    idPictures: "4",
+    original: process.env.PUBLIC_URL + "/assets/pompon.mp4",
+  },
+  {
+    idPictures: "5",
+    original: process.env.PUBLIC_URL + "/assets/kiss.mp4",
+  },
+
+
 ];
