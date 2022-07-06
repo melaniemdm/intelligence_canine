7950b96fbf06b31148aad943c077a47c617119e0
melaniemdm <melanieborja.pro@gmail.com>
2022-07-06 13:57:39 +0200

    typo(js) datapro

--- src/datas/pro.js
+++ src/datas/pro.js
@@ -17,7 +17,7 @@ export const  proTrust= [
   },
     {
     id: "4",
-    namePro: "La Tour de Farmina",
+    namePro: "Elevage de La Tour de Farmina",
     picturePro: process.env.PUBLIC_URL +'/assets/farmina.jpeg',
     ville:'Saint-Phal',
     lien:'http://www.labradorsdelatourfarmina.com/?fbclid=IwAR0hVEHczSVFuklbXpQ9Epe29Wmmgq0TUFMbjquKJod41-_mzd4n-Iv24V8'
